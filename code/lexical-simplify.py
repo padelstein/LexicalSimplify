56bfd87c09aeb51b5b7f3b6c7902a7a10614c394
Patrick Adelstein <padelstein@middlebury.edu>
2012-06-08 16:34:11 -0400

    its friday

--- code/lexical-simplify.py
+++ code/lexical-simplify.py
@@ -1,5 +1,51 @@
 # lexical-simplify.py
 # substitutes words in input sentences
 # Patrick Adelstein, Middlebury College Summer 2012, Prof. David Kauchak
+# adapted from Sujay Kumar Jauhar
 
-def 
+import sys
+
+class Usage(Exception):
+	def __init__(self, msg):
+		self.msg = msg
+
+
+def createFreqHash(file):
+	wordFreq = {}
+
+	for line in file:
+		words_in_line = line.lower().split()	
+		# should add code to remove titles in particular file(simple.txt)
+		for word in words_in_line:
+			if wordFreq.has_key(word):
+				wordFreq[word] += 1
+			else:
+				wordFreq[word] = 1
+
+	return wordFreq
+
+
+#function to read system produced ranking file
+def getSystemRankings(file):
+	#pattern to recognize rankings in output file
+	pattern = re.compile('.*?\{(.*?)\}(.*)')
+	
+	for line in file:
+		rest = line
+		counter = 0
+		while rest:
+			match = pattern.search(rest)
+			currentRank = match.group(1)
+			individualWords = currentRank.split(', ')
+			for word in individualWords:		
+				word = re.sub('\s$','',word)
+				currentContextRanking[word] = counter
+			rest = match.group(2)
+			counter += 1
+		
+		allContextRankings.append(currentContextRanking)
+		
+	return allContextRankings
+
+if __name__ == "__main__":
+
