f1a5c48bc182bde0ed8262e76a66bd7039c27945
Patrick Adelstein <padelstein@middlebury.edu>
2012-06-13 14:31:48 -0400

    did data statistics on simple wiki corpus

--- code/python/frequency-calc.py
+++ code/python/frequency-calc.py
@@ -5,9 +5,15 @@
 import sys
 
 wordFreq = {}
-doc = open(sys.argv[1])
+corpus = open(sys.argv[1])
+normalToSimple = open(sys.argv[2])
+cnt = 0
+normFreq = 0
+simpFreq = 0
+numSent = 0
 
-for line in doc:
+
+for line in corpus:
 	(article, paragraph, text) = line.split("\t")
 	words_in_line = text.lower().split()	
 	for word in words_in_line:
@@ -16,5 +22,24 @@ for line in doc:
 		else:
 			wordFreq[word] = 1
 
-for word in wordFreq:
-	print word + "\t" + str(wordFreq[word])
+for line in normalToSimple:
+	(normal, simple) = line.split("\t")
+	simple = simple.strip('\n').strip()
+	normal = normal.lower()
+	simple = simple.lower()
+	if wordFreq.has_key(simple):	
+		if not wordFreq.has_key(normal):
+			wordFreq[normal] = 0		
+		numSent += 1
+		if wordFreq[simple] > wordFreq[normal]:
+			cnt += 1
+		normFreq += wordFreq[normal]
+		simpFreq += wordFreq[simple]
+	else:
+		print normal + ' ' + simple
+
+normFreq = float(normFreq)/numSent
+
+simpFreq = float(simpFreq)/numSent
+
+print 'The number of good substitutions is ' + str(cnt) + ' and the frequency of normal words is ' + str(normFreq) + ' and the frequency of simple words is ' + str(simpFreq) + ' out of ' + str(numSent)
