d19a799ee18764f3be45411caeba55a64585b782
Patrick Adelstein <padelstein@middlebury.edu>
2012-06-20 14:49:03 -0400

    compared result freqs to simple wiki freqs

---
+++ code/python/frequency-calc.workerOutput.py
@@ -0,0 +1,35 @@
+
+import sys
+
+resultsFreq = {}
+corpusFreq = {}
+results = open(sys.argv[1])
+corpus = open(sys.argv[2])
+
+grossNumberOfWords = 0
+
+#for line in results:
+#	words_in_line = line.split(",")
+#	for word in words_in_line:
+#		word = word.strip().strip("]")
+#		if resultsFreq.has_key(word):
+#			resultsFreq[word] += 1
+#		else:
+#			resultsFreq[word] = 1
+
+for line in corpus:
+	(article, paragraph, text) = line.split("\t")
+	words_in_line = text.lower().split()	
+	for word in words_in_line:
+		if corpusFreq.has_key(word):
+			corpusFreq[word] += 1
+		else:
+			corpusFreq[word] = 1
+	grossNumberOfWords += 1
+
+#for key, value in sorted (resultsFreq.iteritems(), key = lambda (k,v): (v,k) ):
+for line in results:
+	(key, value) = line.split("\t")
+	if not corpusFreq.has_key(key):
+		corpusFreq[key] = 0	
+	print value + "," + str(float(corpusFreq[key])/float(grossNumberOfWords)) 
